cc9fd468c01f56d8f976753d86b184cfbd11f1b9
Yerram Abhilash <abhilashabhi2909@gmail.com>
2024-04-07 23:27:29 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/bubbleSort.java
@@ -0,0 +1,13 @@
+public class bubbleSort {
+    public static void bubbleSort(int n, int ar) {
+
+    }
+    public static void main(String[] args) {
+        int[] ar = {3, 1, 9, 7, 2, 5};
+        bubbleSort(6, ar);
+        for(int i : ar) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
